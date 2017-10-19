02487c4fb5c43bd7d641d59c3972332b8de6ed5b
sim756 <sim756@live.com>
2017-10-19 16:09:36 +0600

    treegui-cmd-win

---
+++ treegui-cmd-win/treegui/treegui/TreeStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace treegui
+{
+    public static class TreeStorage
+    {
+        public static string Path { get; set; }
+        public static bool FlagF { get; set; }
+        public static bool FlagA { get; set; }
+    }
+}
